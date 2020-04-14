00a98c6a7d05f8c85ce9bfbe8748810fbbe397d9
Ramandeep <ramankhural@gmail.com>
2020-04-14 21:44:26 +0530

    allow custom headers to be passed to bot - enable expndo methods for columns via metaprogramming

--- lib/two_pines/google_sheet/worksheet.rb
+++ lib/two_pines/google_sheet/worksheet.rb
@@ -4,23 +4,34 @@ module TwoPines
 module GoogleSheet
 class Worksheet
 
-  attr_accessor :sheet, :ws, :data, :headers, :header_row
+  attr_accessor :sheet, :ws, :data, :headers, :header_row, :header_map
 
   def initialize sheet, worksheet, header_row
     @sheet = sheet
     @header_row = header_row.to_i
     @ws = worksheet
     @headers = @ws.rows[@header_row]
+    set_headers @headers
+  end
+
+  def set_headers header_keys
+    header_values = @headers
+    @header_map = Hash.new
+    @headers.each_with_index do |header, index|
+      @header_map[header_keys[index]] = header
+    end
+    @headers = header_keys
     read_data
   end
 
+
   def read_data
     @data = Array.new
-    @ws.rows
-    for row_number in @header_row+1...@ws.rows.count do
+    raw_rows = @ws.rows
+    for row_number in @header_row+1...raw_rows.count do
       data_row = Hash.new
       headers.count.times do |index|
-        data_row[@headers[index]] = @ws.rows[row_number][index] unless @ws.rows[row_number][index] == ""
+        data_row[@headers[index]] = raw_rows[row_number][index] unless raw_rows[row_number][index] == ""
       end
       if !data_row.empty?
         data_row['__sheet_row_number'] = row_number
